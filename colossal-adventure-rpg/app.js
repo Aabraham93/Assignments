f2343d028c1bf96d214e27bae4e998e362b83967
anthony abraham <anthony_abraham93@hotmail.com>
2023-04-24 11:19:22 -0500

    Rpg was updated and works now along with escape room

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -7,7 +7,12 @@ console.log(`Hi ${playerName},! Time to have some fun!`);
 
 let inventory = []; // Initialize the player's inventory as an empty array
 
-while (true) { // Use a while loop to control the flow of the game while the user keeps walking
+let playerHP = 100;
+
+let game = true
+
+
+while (game) { // Use a while loop to control the flow of the game while the user keeps walking
   let input = readlineSync.question('Enter "w" to go for a walk: ');
 
   if (input === 'w') {
@@ -23,40 +28,50 @@ while (true) { // Use a while loop to control the flow of the game while the use
     }
   } else {
     console.log('Invalid entry.');
+  } 
+  if(playerHP<=0){
+    console.log("GAME OVER")
+    game= !game
   }
 }
 
 function fight() {
   console.log('You are in a battle!');
+  
+  let enemyHP = 100;
 
   // Choose a random enemy from a minimum of 3 different enemy names
   let enemyNames = ['Bear', 'Wolf', 'Mountain Lion'];
   let randomIndex = Math.floor(Math.random() * enemyNames.length);
   let enemyName = enemyNames[randomIndex];
 
-  let playerHP = 100;
-  let enemyHP = 100;
 
-  while (playerHP > 0 && enemyHP > 0) { 
+
+  while ( enemyHP > 0) { 
     // Use a while loop to control the flow of the battle until either the player or the enemy has 0 or less health
     let action = readlineSync.question('Enter "a" to attack or "r" to run: ');
 
     if (action === 'a') { // If the user chooses to attack
-      let playerDamage = Math.floor(Math.random() * (20 - 10 + 1)) + 10; // Generate a random amount of damage dealt between 10 and 20
+      let damageNumbers= Math.floor(Math.random() *20)+10;
+      // console.log(damageNumbers)
+      // Generate a random amount of damage dealt between 10 and 20
+      let playerDamage = Math.floor(Math.random()* damageNumbers); 
       enemyHP -= playerDamage;
       console.log(`You deal ${playerDamage} damage to the ${enemyName}.`);
 
       if (enemyHP <= 0) { // If the enemy dies
         console.log(`You defeated the ${enemyName}!`);
 
+      
+
         // Add an item to the player's inventory and update their HP
         let item = 'BandAid';
         inventory.push(item);
-        playerHP += 50;
+        playerHP += 20;
 
-        console.log(`You found a ${item} and gained 50 HP.`);
+        console.log(`You found a ${item} and gained 20 HP.`);
       } else { // If the enemy is still alive
-        let enemyDamage = Math.floor(Math.random() * (15 - 5 + 1)) + 5; 
+        let enemyDamage = Math.floor(Math.random() * 10) + 1; 
         // Generate a random amount of damage dealt between 5 and 15
         playerHP -= enemyDamage;
         console.log(`The ${enemyName} deals ${enemyDamage} damage to you.`);
@@ -64,13 +79,18 @@ function fight() {
         console.log(`Player HP: ${playerHP}`);
         console.log(`${enemyName} HP: ${enemyHP}`);
       }
+
+      if (playerHP<=20) {
+      
+        console.log("You've lost this fight.")
+      }
     } else if (action === 'r') { // If the user chooses to run
       let randomNum = Math.random();
       if (randomNum <= 0.5) { // Use a 50% chance of escaping
-        console.log('You successfully ran away.');
+        console.log('You ran away.');
         return; // End the battle
       } else { // If the escape attempt fails
-        let enemyDamage = Math.floor(Math.random() * (15 - 5 + 1))
+        let enemyDamage = Math.floor(Math.random() *10 ) + 1
 
       }
     }
--- colossal-adventure-rpg/escape.js
+++ colossal-adventure-rpg/escape.js
@@ -5,34 +5,36 @@ console.log(`Hi ${name}, Let's try to find the key so we can escape!`)
 //List places for player to look.
 const start = ['Find the key', 'Open the door','Put hand in hole']
 
-for(let i = 0; i <= 3; i++){
+// for(let i = 0; i <= 3; i++){
 
    
 //set if key is found or not what happens in loop
 let foundKey = false;
-
+let trapped = true;
 // the exclamation point makes it loop through until foundkey is true 
-while(!foundKey){
+while(trapped){
 //Let them pick where they want to look.
    index = readline.keyInSelect(start, `What do you want to do first?`);
    if(index === 2 ){
     console.log(`Oh No! You're Dead`)
+    trapped = !trapped
     break ; //exit loop 
 }   
     else if(index === 0){
     console.log("You found the key! Now you can escape!");
-    foundKey=true
-    break;
+    foundKey=true;
 
-}   else if(index ===1){
-    console.log("The door is locked, find the key first!")
-    break;
+}   else if(index ===1 ){
+    if (foundKey === true) {
+        console.log("You've escaped! Congrats!")
+        trapped = !trapped
+    } else {
+    console.log("The door is locked, find the key first!")  };
 
-}if(foundKey=true){
-    console.log("You've escaped! Congrats!")
 }
 }
-}
+
+
 
     
 
