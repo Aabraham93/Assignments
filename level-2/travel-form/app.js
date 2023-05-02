c22c64fd1a04e9b82b8c294688bdb724b9e85905
Anthony Abraham <anthony_abraham93@hotmail.com>
2023-05-01 22:23:11 -0500

    Completed Travel Form

--- level-2/travel-form/app.js
+++ level-2/travel-form/app.js
@@ -1,21 +1,39 @@
-const form = document.querySelector("#userInfo")
+const form = document.userInfo
 
 const submit = document.querySelector("#submit");
 
 
-function formAlert(){
+function formAlert(e){
+
+    e.preventDefault()
+
     const firstName = form.firstName.value ;
     const lastName= form.lastName.value;
     const age= form.age.value;
     const location= form.locations.value;
     const diet= [];
 
+  
+    
+  for(i=0; i< form.food.length;i++){
 
-    ///add comment
-    // if(){
-
-    // }
-
+      if(form.food[i].checked){
+      diet.push(form.food[i].value)
+      }
+  }
+  function dietList() 
+  {let list= ""
+    for(i=0; i< diet.length; i++){
+        list= list+diet[i]+ ". "
+    }
+    return list
+  }
+alert(
+    `First name: ${firstName} \n Last Name: ${lastName} \n Age: ${age} \n Location: ${location} \n Diet: ${dietList()}`
+)
     
 }
 submit.addEventListener("click",formAlert)
+
+
+// list = 'vegan '
